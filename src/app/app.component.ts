55cd7d967aa471ce2401a79dab81ac7d522de1b2
Jinkogule <lucaspimenta21@gmail.com>
2024-05-21 20:20:35 -0300

    add get description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,17 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   pokemon: any;
+  description: string | undefined;
 
   constructor(private pokeApiService: PokeApiService) {
-    this.pokeApiService.getPokemon('chandelure').subscribe(data => {
+    this.pokeApiService.getPokemon('pikachu').subscribe(data => {
       this.pokemon = data;
+    });
+
+    this.pokeApiService.getDescription('pikachu').subscribe(data => {
+      const flavorTextEntries = data.flavor_text_entries;
+      const englishFlavorTextEntries = flavorTextEntries.filter((entry: { language: { name: string; }; }) => entry.language.name === 'en');
+      this.description = englishFlavorTextEntries[0].flavor_text;
     });
   }
 }
--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/';
+  private baseUrl = 'https://pokeapi.co/api/v2';
 
   constructor(private http: HttpClient) { }
 
-  getPokemon(name: string) {
-    return this.http.get(this.url + name);
+  getPokemon(name: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon/${name}`);
+  }
+
+  getDescription(name: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/pokemon-species/${name}`);
   }
 }
