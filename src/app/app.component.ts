226a157cc6f70196aa78d0d9ceae446fbbc85edc
Jinkogule <lucaspimenta21@gmail.com>
2024-05-29 14:18:05 -0300

    add menu de links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonApp, IonRouterOutlet, IonHeader, IonFooter, IonContent } from '@ionic/angular/standalone';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, IonApp, IonRouterOutlet, IonHeader, IonFooter, IonContent],
+  imports: [IonicModule],
   standalone: true,
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
