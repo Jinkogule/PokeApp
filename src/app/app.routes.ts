4b6dbe56503e7f4a193b0babfe4e2742636bc9d2
Jinkogule <lucaspimenta21@gmail.com>
2024-07-25 10:08:00 -0300

    removendo lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from '@angular/router';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { FavoritePokemonsComponent } from './favorite-pokemons/favorite-pokemons.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pokedex/pokedex.component').then( m => m.PokedexComponent)
+    component: PokedexComponent
   },
   {
     path: 'pokemon-details/:name',
-    loadComponent: () => import('./pokemon-details/pokemon-details.component').then( m => m.PokemonDetailsComponent)
+    component: PokemonDetailsComponent
   },
   {
     path: 'favorite-pokemons',
-    loadComponent: () => import('./favorite-pokemons/favorite-pokemons.component').then( m => m.FavoritePokemonsComponent)
+    component: FavoritePokemonsComponent
   },
 ];
